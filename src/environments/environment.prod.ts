13afb8950e45254904e24eee7929d74151965f74
Bessam <ssahli@wissalgroup.com>
2021-04-06 17:02:25 +0100

    update clientId for oath

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   host : 'https://u9v7cu8i98.execute-api.eu-west-3.amazonaws.com',
-  loginUrl: 'https://service-user-pool-domain-prod-wi-keys.auth.eu-west-3.amazoncognito.com/login?response_type=token&client_id=4mabv454844p2bubih20si3fjk&redirect_uri=https://wi-keys.com/login',
+  loginUrl: 'https://service-user-pool-domain-prodv2-wi-keys.auth.eu-west-3.amazoncognito.com/login?response_type=token&client_id=647qnojr6qodfcths8t4q7go3b&redirect_uri=https://wi-keys.com/login',
 };
