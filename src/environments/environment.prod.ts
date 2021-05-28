60c0259ba5ef77b2c90fb3a81fb096c892314818
Bessam <ssahli@wissalgroup.com>
2021-05-28 12:08:54 +0100

    fix redirect url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   host : 'https://u9v7cu8i98.execute-api.eu-west-3.amazonaws.com',
-  loginUrl: 'https://service-user-pool-domain-prodv2-wi-keys.auth.eu-west-3.amazoncognito.com/login?response_type=token&client_id=647qnojr6qodfcths8t4q7go3b&redirect_uri=http://www.wi-keys.com.s3-website.eu-west-3.amazonaws.com/login',
+  loginUrl: 'https://service-user-pool-domain-prodv2-wi-keys.auth.eu-west-3.amazoncognito.com/login?response_type=token&client_id=647qnojr6qodfcths8t4q7go3b&redirect_uri=https://wi-keys.com/login',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,5 +6,5 @@
 export const environment = {
   production: false,
   host : 'https://u9v7cu8i98.execute-api.eu-west-3.amazonaws.com',
-  loginUrl: 'https://service-user-pool-domain-prod-wi-keys.auth.eu-west-3.amazoncognito.com/login?response_type=token&client_id=4mabv454844p2bubih20si3fjk&redirect_uri=http://www.wi-keys.com.s3-website.eu-west-3.amazonaws.com/login',
+  loginUrl: 'https://service-user-pool-domain-prod-wi-keys.auth.eu-west-3.amazoncognito.com/login?response_type=token&client_id=4mabv454844p2bubih20si3fjk&redirect_uri=https://wi-keys.com/login',
 };
